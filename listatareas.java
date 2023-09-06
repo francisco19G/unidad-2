a2cae45fdd11afebffdb119a20cd92049c2f55a0
francisco19G <143814713+francisco19G@users.noreply.github.com>
2023-09-06 08:39:57 -0600

    Add files via upload

---
+++ listatareas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class listatareas {
+    public static void main (String [] arg){
+    String tarea1 = "Tender la cama";
+    String tarea2 = "Hacer el desayuno";
+    String tarea3 = "Lavar los platos";
+    String tarea4 = "Pasear al perro";
+    String tarea5 = "Alimentar al perro";
+    
+    System.out.println("la primer tarea a realizar es " + tarea1);
+    System.out.println("la segunda tarea a realizar es " + tarea2);
+    System.out.println("la tercer tarea a realizar es " + tarea3);
+    System.out.println("la cuarta tarea a realizar es " + tarea4);
+    System.out.println("la ultima tarea a realizar es " + tarea5);
+                          
+}
+}
