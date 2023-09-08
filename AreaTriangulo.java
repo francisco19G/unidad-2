5309b5d0d1611a50e25658b3b0aefc96e468d886
francisco19G <143814713+francisco19G@users.noreply.github.com>
2023-09-08 08:51:54 -0600

    Add files via upload

---
+++ AreaTriangulo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package areatriangulo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AreaTriangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int altura = 8;
+        int base = 10 ;
+        int area = (base * altura)/2;
+        System.out.println("Ela area del rectamgulo es: " + area);
+    }
+    
+}
